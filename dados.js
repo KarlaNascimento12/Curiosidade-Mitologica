ceb6d4929cdc9e1d9dbab0c3b1bb70f9c1cd3ac5
KarlaNascimento12 <142931446+KarlaNascimento12@users.noreply.github.com>
2024-09-07 05:01:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,257 @@
+let dados = [
+    {
+        titulo: "Mitologia Grega",
+        descricao: "A Mitologia Grega reúne um conjunto de lendas e mitos que foram criados pelos gregos na antiguidade. O objetivo principal era de explicar alguns fatos, como a origem da vida, a vida após a morte, ou até mesmo os fenômenos da natureza.",
+        link:"https://www.todamateria.com.br/mitologia-grega/"
+        
+    },
+    
+    {
+        titulo: "Mitologia Egípcia",
+        descricao: "A mitologia egípcia reúne diversos mitos, lendas e histórias que fizeram parte do imaginário religioso no Egito Antigo até a chegada do Cristianismo.",
+        link: "https://www.todamateria.com.br/mitologia-egipcia/"
+    },
+
+    {
+        titulo: "Mitologia Japonesa",
+        descricao:"Como quase toda história mitológica, as histórias da mitologia japonesa contam e tentam explicar a criação do mundo, o surgimento dos deuses e a origem dos imperadores japoneses. As histórias estão em dois livros, um se chama OKOJIKI e o outro NIHONSHOKI, ainda assim, os dois divergem em vários pontos.",
+        link: "https://www.infoescola.com/mitologia/mitologia-japonesa/"
+    },
+
+    {
+        titulo: "Semideuses, o que são? ",
+        descricao:"De acordo com a mitologia, semideuses são filhos de deuses com mortais. Dessa forma, diferentes dos humanos comuns, os semideuses costumam apresentar características da divindade a qual é descendente.",
+        link:"https://segredosdomundo.r7.com/semideuses/"
+    },
+    {
+        titulo: "Afrodite",
+        descricao: "Na mitologia grega, Afrodite é a deusa do amor, da beleza e da sexualidade. Ela foi considerada a personificação do ideal de beleza dos gregos na Antiguidade. E, na Idade Moderna, serviu de inspiração para diversos artistas do Renascimento.",
+        link: "https://www.todamateria.com.br/deusa-afrodite/",
+        tags: "amor deusa"
+    },
+    {
+        titulo: "Apolo" ,
+        descricao:"Apolo é um deus da mitologia greco-romana, considerado um dos maiores deuses do Olimpo.Ele é venerado como o deus da luz, da profecia, da poesia, das artes, da música, da cura, da juventude, da verdade, do tiro ao alvo e da peste." ,
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        titulo: "Ares",
+        descricao: "Ares é o deus grego da guerra. Filho de Zeus e Hera, é um dos 12 deuses do Olimpo. Amante de Afrodite, a deusa do amor, Ares era detestado pelos pais e impopular até mesmo entre gregos, sendo considerado perigoso pelo comportamento insaciável no campo de batalha." ,
+        link: "https://www.todamateria.com.br/deus-ares/"
+    },
+
+    {
+        titulo: "Artemis" ,
+        descricao: "Ártemis é a deusa da caça, da vegetação, da castidade, do parto e dos animais selvagens. É uma das mais veneradas divindades da mitologia grega e, na mitologia romana, é chamada de Diana." ,
+        link: "https://www.todamateria.com.br/artemis/"
+    },
+
+    {
+        titulo: "Atena",
+        descricao: "Atena é a deusa grega da sabedoria, das artes, da inteligência, da guerra e da justiça. Considerada protetora das cidades, dos arquitetos, dos tecelões e dos ourives, ela foi cultuada em toda a Grécia Antiga" ,
+        link: "https://www.todamateria.com.br/deusa-grega-atena/"
+    },
+    
+    {
+        titulo: "Poseidon",
+        descricao:"Poseidon é o deus grego do mar, dos terremotos, das tempestades e dos cavalos. Protetor das águas e auxiliar dos marinheiros, também era chamado de deus da fertilidade.",
+        link: "https://www.todamateria.com.br/poseidon/"
+    },  
+
+    {
+        titulo:"Zeus",
+        descricao: "Zeus é uma divindade mitológica grega. É considerado o senhor dos deuses e dos homens que habitavam o monte Olimpo na Grécia antiga. As divindades mitológicas tinham nas mãos o destino dos homens. Regiam o mundo e conduziam o espetáculo da vida.",
+        link: "https://www.todamateria.com.br/zeus/"
+    },
+
+    {
+        titulo: "Hera",
+        descricao: "A deusa Hera era a rainha do Olimpo, também chamada de rainha do paraíso. Deusa do nascimento e do casamento, Hera é o símbolo da monogamia, da fidelidade conjugal e da fertilidade. Protetora dos nascimentos e das mulheres no casamento, diversos templos foram erguidos em sua homenagem.",
+        link: "https://www.todamateria.com.br/deusa-hera/"
+    },
+
+    {
+        titulo: "Rá-Atum",
+        descricao: "Rá (do português Ré) é o Deus Egípcio do Sol sendo a principal divindade da religião egípcia. O culto ao Deus Sol foi muito próspero no Egito, sendo a principal forma de adoração e um culto oficial por cerca de vinte séculos.",
+        link: "https://www.todamateria.com.br/ra-deus-do-sol/"
+    },
+
+    {
+        titulo: "Osíris",
+        descricao: "Osíris é o deus do julgamento, do além e da vegetação, sendo considerado um dos mais importantes e populares da mitologia egípcia. Os cultos a Osíris eram muito comuns e são registrados a partir de 2400 a.C. Por esse motivo, diversos templos foram erigidos em sua homenagem.",
+        link: "https://www.todamateria.com.br/osiris/"
+    },
+
+    {
+        titulo:"Perseu",
+        descricao:"Perseu, filho de Zeus e Danai, é um dos heróis mais emblemáticos da mitologia grega, considerado um semideus. Seu pai, Zeus, é o Deus dos Deuses e, portanto, o principal da mitologia grega.",
+        link: "https://www.todamateria.com.br/perseu/"
+    },
+
+    {
+        titulo:"Odin",
+        descricao: "Odin é um deus nórdico, senhor da cura, da vida e da morte. Seu culto se espalhou pelo norte da Europa e é chamado de Wotan na mitologia germânica. Retratado como um homem velho, de longas barbas brancas, mas robusto, Odin por vezes aparece vestido como simples peregrino ou com suas armas de guerreiro.",
+        link: "https://www.todamateria.com.br/odin/"
+    },
+
+    {
+        titulo: "Thor",
+        descricao: "Thor é um deus da mitologia nórdica associado aos trovões e às batalhas. Seu culto era muito popular entre os camponeses. É preciso esclarecer que existem poucas fontes primárias sobre Thor e assim, várias informações sobre sua vida foram acrescidas e omitidas ao longo do tempo.",
+        link: "https://www.todamateria.com.br/thor/"
+    
+    },
+
+    {
+        titulo:"Teseu",
+        descricao: "Teseu, filho de Egeu e Etra, é considerado um dos maiores heróis da mitologia grega. Sua grande importância está relacionada com sua força, valentia e com a morte do Minotauro. Na língua grega, seu nome significa “ homem forte”.",
+        link: "https://www.todamateria.com.br/teseu/"
+    },
+
+    {
+        titulo: "Aquiles",
+        descricao:"Aquiles, filho de Peleu e Tétis, é um herói e semideus da mitologia grega. Ele era muito belo, forte e corajoso. Considerado um dos maiores guerreiros mitológicos da Grécia Antiga, Aquiles participou de várias batalhas, entretanto, a mais importante foi a Guerra de Troia.",
+        link:"https://www.todamateria.com.br/aquiles/",
+        tags:"belo" 
+    },
+
+    {
+        titulo:" Hercules",
+        descricao: "Hércules ou Herácles é um dos mais notáveis heróis e semideuses da mitologia greco-romana. Com um aspecto viril, ele foi um grande guerreiro.",
+        link: "https://www.todamateria.com.br/hercules/",
+        tags: "força"
+    },
+{
+    titulo:"Centauros",
+    descricao:"Os centauros formam uma classe de monstros da mitologia grega que possuem o corpo metade humano, metade cavalo. Segundo a concepção do pensamento mítico grego, o cavalo possuía uma série de virtudes admiráveis e, por isso, a sua mistura com os homens não era vista como algo negativo. Diversas narrativas mitológicas contam sobre o relacionamento entre os homens e os centauros, o que conferia a esse monstro a capacidade de socialização. ",
+    link: "https://brasilescola.uol.com.br/mitologia/centauros.htm",
+    tags: "cavalo"
+},
+
+{
+    titulo:"Caixa de Pandora",
+    descricao: "A Caixa de Pandora é uma narrativa da mitologia e da religiosidade grega que explica como o mal e as doenças se espalharam pelo planeta. Pandora era uma mulher, a primeira a existir, sendo criada por Hefesto com base em instruções dadas por Zeus, o principal deus do panteão grego.",
+    link:"https://brasilescola.uol.com.br/mitologia/a-caixa-pandora.htm"
+},
+
+{
+    titulo:"Origem da Mitologia",
+    descricao: "Para explicar de onde veio a mitologia, como surgiu e as verdades e fantasias, os filósofos propuseram algumas teorias que comprovam a origem dos acontecimentos e de seres mitológicos. • Teoria da Escritura: Afirma que as lendas mitológicas são derivadas de das Escrituras Sagradas (bíblia) com alguns retoques e alterações. ",
+    link:"https://brasilescola.uol.com.br/mitologia/origem-mitologia.htm"
+},
+
+{
+titulo:"Unicórnio",
+descricao:"O unicórnio é uma figura mitológica originária do Oriente. Sua história, provavelmente, foi trazida por mercadores que faziam a rota do Oriente com a Europa.",
+link: "https://www.todamateria.com.br/unicornio/"
+},
+
+{
+    titulo: "Fenix",
+    descricao:"A Fênix é uma ave mitológica que representa os ciclos da vida, o recomeço e a esperança num futuro melhor. De origem egípcia, o mito está presente em várias culturas como a grega, romana, árabe e chinesa.",
+    link: "https://www.todamateria.com.br/fenix/"
+},
+
+{
+    titulo:"Medusa",
+    descricao: "Na mitologia grega, Medusa era uma criatura representada por uma mulher com serpentes no lugar dos cabelos, presas de bronze e asas de ouro. Simbolicamente, Medusa era trágica, solitária e figura de uma mulher incapaz de amar e ser amada. Odiava os homens por ter sido seduzida e também odiava as mulheres, pois não se conformava em ser uma criatura monstruosa.",
+    link: "https://www.todamateria.com.br/o-mito-de-medusa-na-mitologia-grega/"
+},
+
+{
+    titulo: "Temis",
+    descricao: "Na mitologia grega, Têmis é a personificação das leis divinas, reconhecida pelo Costume e pela Lei. Em contraste à justiça, leis e decretos humanos. Contudo, é vista como a deusa guardiã dos juramentos dos homens e das suas regras. Era comum ser  mencionada durante os juramentos dos magistrados.",
+    link: "https://segredosdomundo.r7.com/temis/"
+},
+
+{
+    titulo: "QUIMERA ",
+    descricao:"A Quimera, na mitologia grega, era uma fera que soltava fogo pelas narinas. Surgiu na Grécia Antiga, por volta do século VII a.C. Era representada, principalmente, por uma mistura entre o corpo de leão e de cabra.",
+    link: "https://www.suapesquisa.com/mitologiagrega/quimera.htm"
+},
+
+{
+    titulo: "Mitologia Africana ",
+    descricao: "A mitologia dos iorubás é responsável por englobar tanto a visão de mundo, quanto a religião deste povo. Além da África, principalmente na Nigéria, ela se faz presente em países das américas para onde os africanos escravizados foram trazidos, a exemplo do Brasil e Cuba. Ao contrário do que muitas pessoas pensam, a mitologia iorubá é monoteísta, sendo que o deus supremo e o criador de tudo é Olorum.",
+    link: "https://escolaeducacao.com.br/mitologia-africana/"
+},
+
+{
+    titulo: "Mitologia Indigena Brasileira",
+    descricao: "O Brasil é um país marcado pela sua incrível diversidade cultural, e uma das suas maiores riquezas é a presença de inúmeras etnias indígenas, cada uma com suas próprias tradições, histórias e mitologias. A mitologia indígena brasileira é um vasto e fascinante campo de estudo que revela muito sobre a visão de mundo, valores e cosmologias dessas culturas.",
+    link: "https://www.mitoselendas.com.br/2024/07/mitologia-indigena-brasileira.html"
+},
+
+{
+    titulo: "Mitologia Nordica",
+    descricao: "A mitologia nórdica é o conjunto de crenças, lendas e mitos que compunham a religião dos chamados povos escandinavos. São as narrativas que marcaram a Era dos Vikings (século VIII ao XI d.C.), que habitavam a região atualmente abrangida pela Suécia, Dinamarca, Noruega e Islândia, principalmente.",
+    link: "https://www.significados.com.br/mitologia-nordica/"
+},
+
+{
+    titulo: "Mitologia Romana",
+    descricao: "A Mitologia Romana é o conjunto de crenças e práticas politeístas, ou seja adorando vários deuses com nomes diferentes dos deuses gregos, mas semelhante aos da Mitologia Grega. Para os Romanos, homens e deuses teriam que viver em harmonia, tendo confiança mútua, sendo que os rituais e cultos aos deuses tinham como objetivo agradá-los, pois dos deuses dependiam a saúde, a proteção do Estado e o sucesso na guerra, as colheitas fartas, enfim, a prosperidade dos homens.",
+    link: "https://www.infoescola.com/mitologia/mitologia-romana/"
+},
+
+{
+    titulo: "Mitologia chinesa",
+    descricao: "A mitologia chinesa é o conjunto de histórias, lendas e ritos da China passados de geração para geração de forma oral ou escrita. Há diversos temas na mitologia chinesa, incluindo mitos envolvendo a fundação da cultura chinesa e do Estado chinês. Como em muitas mitologias, acredita-se que ela seja uma forma de rememoração de fatos passados.",
+    link: "https://pt.wikipedia.org/wiki/Mitologia_chinesa"
+},
+
+{
+    titulo: "Mitologia Celta",
+    descricao: "Os celtas, segundo alguns estudiosos um conjunto de povos, para outros uma raça, estenderam-se por toda a Europa, contribuindo para a constituição de várias outras nações. O núcleo de sua localização foi provavelmente a Alemanha, mas os chamados continentais se disseminaram pela Hungria, Grécia e pela Ásia Menor. Eles nunca formaram um bloco homogêneo, e sim tribos rivais, que nem mesmo cultuavam as mesmas divindades, com exceção de poucas entidades comuns.",
+    link: "https://www.infoescola.com/mitologia/mitologia-celta/"
+},
+
+{
+    titulo: "Sereias",
+    descricao: "Na Mitologia Grega, são seres metade mulher e metade peixe (ou pássaro, segundo alguns escritores antigos) capazes de atrair e encantar qualquer um que ouvisse o seu canto." ,
+    link: "https://www.infoescola.com/mitologia-grega/sereias/"
+},
+
+{
+    titulo:"Ninfa",
+    descricao: "Ninfa é um termo que tem origem na mitologia grega, sendo utilizado para descrever seres divinos femininos, geralmente associados à natureza e à fertilidade. Nas histórias mitológicas, as ninfas eram consideradas deidades menores, habitando florestas, rios, montanhas e outros locais naturais.",
+    link: "https://resumos.soescola.com/glossario/o-que-e-ninfa/"
+},
+
+{
+    titulo: "Hefesto",
+    descricao: "Hefesto é o deus do fogo, dos metais e da metalurgia na mitologia grega. Sua figura também estava associada ao trabalho, pois foi um grande forjador e joalheiro. Na mitologia romana, ele é chamado de Vulcano.",
+    link: "https://www.todamateria.com.br/hefesto/"
+},
+
+{
+    titulo: "Hermes",
+    descricao: "Hermes foi uma divindade da religiosidade dos gregos antigos, sendo o deus do comércio, da riqueza, da sorte, do sono, dos ladrões, da diplomacia, das viagens, entre outros atributos. Era tido como inteligente, furtivo e capaz de pregar algumas peças eventualmente.",
+    link: "https://brasilescola.uol.com.br/mitologia/hermes.htm"
+},
+
+{
+    titulo: "Basilisco",
+    descricao: "Descrito em diversos dos bestiários medievais, o basilisco é uma criatura designada como o “rei das serpentes” e teria habilidades letais, podendo matar alguém com um simples olhar. Ao que indica essas mesmas lendas, este animal incomum era reconhecido por sua crista em forma de coroa que lhe conferia a posição distinta em relação aos demais répteis.",
+    link: "https://brasilescola.uol.com.br/mitologia/basilisco.htm"
+
+},
+
+{
+    titulo: "Proteu",
+    descricao: "Era filho do Oceano e de Tetis e marido de uma ninfa marinha que era sua irmã. O deus marinho e o mais velho de todos, não é a imagem do mar agitado, mas sim do mar calmo e sedutor. É simbolizado na imagem de um ancião, de fisionomia severa e pacífica, pleno de justiça e de moderação.",
+    link: "https://brasilescola.uol.com.br/mitologia/proteu.htm"
+},
+
+{
+    titulo: "Eolo",
+    descricao: "O deus Éolo vivia numa ilha flutuante na Eólia com seus doze filhos. Foi presenteado por Zeus e recebeu o poder de despertar e acalmar os ventos tornando-se o deus dos ventos. Odisseu, herói grego, indo visitá-lo foi presenteado por Éolo com uma sacola de couro com todos os ventos dentro. Foi recebido como convidado de honra.",
+    link: "https://brasilescola.uol.com.br/mitologia/eolo.htm"
+},
+
+{
+    titulo: "Gaia",
+    descricao: "Gaia, a segunda divindade primordial segundo a mitologia grega, surgiu apenas depois de Caos, representando a Terra. Com uma enorme potencialidade geradora, Gaia gera pontos, montanhas e seu principal filho e posterior esposo, Urano.",
+    link: "https://brasilescola.uol.com.br/mitologia/gaia.htm"
+}
+
+];
